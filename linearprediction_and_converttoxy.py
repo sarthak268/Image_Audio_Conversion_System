99d55ccd2316926bce0c83ecc7e549de083a76c0
sarthak268 <sarthakbhagat@Sarthaks-MacBook-Pro.local>
2017-10-20 02:09:00 +0530

    start

---
+++ linearprediction_and_converttoxy.py
@@ -0,0 +1,28 @@
+
+def lr2xy(l):
+	if (len(l)%2):
+		m = len(l)
+	else :
+		m = len(l) - 1
+
+	new = []
+	for i in (0,2,m):	
+		left = l[i]
+		right = l[i+1]
+		x = (left + right)/2
+		y = left - right
+		new.append([x,y])
+
+	return new 
+
+def apply_linear_prediction(l):
+	new = []
+	for i in range(len(l)):
+		if (i == 0 or i == 1):
+			new.append(l[i])
+		else:
+			predicted_x = 2*l[i-1][0] - l[i-2][0]
+			predicted_y = 2*l[i-1][1] - l[i-2][1]
+			diff_x = l[i][0] - predicted_x
+			diff_y = l[i][1] - predicted_y
+			new.append([diff_x,diff_y])
