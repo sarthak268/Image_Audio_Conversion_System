e80ccd97ad698e648bf2c54a61382689a9aa7084
sarthak268 <sarthakbhagat@Sarthaks-MacBook-Pro.local>
2017-10-20 14:05:01 +0530

    predicted and lr2xy checked

--- linearprediction_and_converttoxy.py
+++ linearprediction_and_converttoxy.py
@@ -1,9 +1,9 @@
 
 def lr2xy(l):
-	if (len(l)%2):
-		m = len(l)
+	if (len(l)%2 == 0):
+		m = len(l) - 2
 	else :
-		m = len(l) - 1
+		m = len(l) - 3
 
 	new = []
 	for i in (0,2,m):	
@@ -21,8 +21,17 @@ def apply_linear_prediction(l):
 		if (i == 0 or i == 1):
 			new.append(l[i])
 		else:
+			#####################################
 			predicted_x = 2*l[i-1][0] - l[i-2][0]
 			predicted_y = 2*l[i-1][1] - l[i-2][1]
+			#####################################
 			diff_x = l[i][0] - predicted_x
 			diff_y = l[i][1] - predicted_y
 			new.append([diff_x,diff_y])
+	return new
+
+y = [1,2,3,4,5,6,7,8,9,10]
+x=lr2xy(y)
+print (x)
+z=apply_linear_prediction(x)
+print(z)
